7c3e7651de55a963c7371c4f2bc90c7ca759647f
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2024-09-02 11:13:07 -0600

    size min file audience

--- Assets/Scripts/Entities/Players/PlayerCamera.cs
+++ Assets/Scripts/Entities/Players/PlayerCamera.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
@@ -7,7 +8,7 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField] private float sensX;
     [SerializeField] private float sensY;
 
-    public Transform orientation;
+    // public Transform orientation;
 
     float xRotation;
     float yRotation;
@@ -24,16 +25,26 @@ public class PlayerCamera : MonoBehaviour
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseY;
-        xRotation += mouseX;
+        yRotation += mouseX;
+        xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        // if (orientation != null)
+        //     orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 
-    public void SetNewParent(Component sender, object data)
+    public void onPlayerSpawns(Component sender, object data)
     {
-        Debug.Log(data);
+        if (sender is BasePlayer player)
+        {
+            if (player.IsOwner)
+            {
+                // orientation = player.transform;
+                transform.parent = player.transform;
+                transform.position = Vector3.zero;
+                Debug.LogWarning("Is Owner!");
+            }
+        }
     }
 }
